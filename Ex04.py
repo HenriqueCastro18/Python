25bcb3870fc2005af39cea4ad99d383507536ee1
HenriqueCastro18 <111105294+HenriqueCastro18@users.noreply.github.com>
2022-11-02 21:50:43 -0300

    Create Ex04.py

---
+++ Ex04.py
@@ -0,0 +1,8 @@
+#1. Vc quer simular a opção de jogar uma moeda e resultar em cara ou coroa
+    #Jogue as opções em uma variável e depois crie um programa que imprime se é 'cara' ou 'coroa' na tela
+
+import random
+
+lado_moeda = ('cara','coroa')
+
+print(random.choice(lado_moeda))
